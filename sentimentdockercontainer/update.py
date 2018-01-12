c822b12cb201560f8952d7a2b3b06a9b95c7a7ef
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2018-01-12 12:50:55 +0100

    Fix

--- sentimentdockercontainer/update.py
+++ sentimentdockercontainer/update.py
@@ -1,41 +1,45 @@
 from flask import Flask, request, Response, abort
 import pandas as pd
-import numpy as np 
-import json 
+import numpy as np
+import json
 
 print("Starting update analysis.")
 
-with open("text6.txt") as f:
+with open("Text6.txt") as f:
     content = f.readlines()
 line2 = "Test"
-df_items =  pd.read_csv("result.csv")
+df_items = pd.read_csv("result.csv")
 
-def RunUpdateAnalysis(text):
-    for line in text:
+
+def run_update_analysis(text):
+    items = []
+    for line in text.splitlines():
         for item in df_items["name"].unique():
             if item.upper() in line.upper():
-                line2 = "Item: " + item +" Line: " + line
-                return line2
-                print(" ")
+                item_change = ''
                 if "DROP-RATE" in line.upper():
-    
-                    line2 = line+" Drop-rate change"
+                    item_change = "drop-rate change"
                 elif "INCREASED" in line.upper():
                     if "DROP" in line.upper():
-                        line2=item + ": Will probably go down"
+                        item_change = "price decrease"
                 elif "DECREASED" in line.upper():
-                    line2=item + ": Will probably go up"
-                 
+                    if "DROP" in line.upper():
+                        item_change = "price increase"
+
+                items.append({'item_name': str(
+                    item), 'line': str(line), 'change': item_change})
+    return items
+
 
 app = Flask(__name__)
 
+
 @app.route('/run-updatescan', methods=['POST'])
 def Start():
-	
-		
-	string = request.data
-        
-	return string
-	
+    string = request.data.decode("utf-8")
+    # return string
+    return json.dumps(run_update_analysis(string))
+
+
 if (__name__ == '__main__'):
-	app.run(port=5000)
+    app.run(port=5000)
