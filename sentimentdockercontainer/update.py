0b62fe060fc6fbdc7ca6f51e88f71fb0c7292710
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2018-01-15 14:21:32 +0100

    fix

--- sentimentdockercontainer/update.py
+++ sentimentdockercontainer/update.py
@@ -5,14 +5,18 @@ from flask import Flask
 
 print("Starting update analysis.")
 
-with open("Text6.txt") as f:
-    content = f.readlines()
-line2 = "Test"
-df_items = pd.read_csv("result.csv")
-
 
 def run_update_analysis():
-    response = requests.post('http://192.168.27.170/graphql', json={'query': '''
+    items_response = requests.post('http://192.168.27.170/graphql', json={'query': '''
+    {
+        items {
+            id
+            name
+        }
+    }
+    '''})
+
+    updates_response = requests.post('http://192.168.27.170/graphql', json={'query': '''
     {
     updates {
         id
@@ -23,11 +27,17 @@ def run_update_analysis():
     }
     '''})
 
-    last_update = json.loads(response.text)["data"]["updates"][0]
+    item_names = [item for item in json.loads(
+        items_response.text)["data"]["items"]]
+
+    removable_items = ["poison", "fur", "pot", "gin",
+                       "weeds", "casket", "hair"]
+
+    last_update = json.loads(updates_response.text)["data"]["updates"][0]
     items = []
     for line in last_update['content'].splitlines():
-        for item in df_items["name"].unique():
-            if item.upper() in line.upper():
+        for item in item_names:
+            if item["name"].upper() in line.upper():
                 item_change = ''
                 if "DROP-RATE" in line.upper():
                     item_change = "drop-rate change"
@@ -37,9 +47,14 @@ def run_update_analysis():
                 elif "DECREASED" in line.upper():
                     if "DROP" in line.upper():
                         item_change = "price increase"
+                items.append({'item_id': item["id"], 'item_name': str(
+                    item["name"]), 'line': str(line), 'change': item_change})
 
-                items.append({'item_name': str(
-                    item), 'line': str(line), 'change': item_change})
+    for removable_item in removable_items:
+        for final_item in items:
+            if removable_item.upper() in final_item["item_name"].upper():
+                items.remove(final_item)
+
     return items
 
 
